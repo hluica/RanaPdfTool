fb1472d3872205eeef0d8da52315131d3bda5b59
hluica <whyzcxl2002@outlook.com>
2026-02-28 19:27:18 +0800

    patch: 检测主题色时，读取更合理的注册表值。

--- Utils/ColorHelper.cs
+++ Utils/ColorHelper.cs
@@ -12,20 +12,18 @@ public static class ColorHelper
     {
         // 1. 检测是否为 Windows 平台
         if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
             return defaultColor;
-        }
 
         try
         {
             using var key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\DWM");
-            if (key?.GetValue("ColorizationColor") is int colorValue)
+            if (key?.GetValue("AccentColor") is int colorValue)
             {
-                // 注册表中的格式是 0xAARRGGBB
+                // 注册表中的格式是 0xffBBGGRR
                 // 我们需要提取 R, G, B 分量
-                byte r = (byte)((colorValue >> 16) & 0xFF);
+                byte r = (byte)((colorValue >> 0) & 0xFF);
                 byte g = (byte)((colorValue >> 8) & 0xFF);
-                byte b = (byte)(colorValue & 0xFF);
+                byte b = (byte)((colorValue >> 16) & 0xFF);
 
                 return new Color(r, g, b);
             }
